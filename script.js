f5a547755d422df009e55bcfd811c1fbdb8554f3
anthonyruiz3 <ant.ruiz2681@gmail.com>
2026-03-06 22:08:23 +0000

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Script started")
+
+function changeColor() {
+    console.log("click");
+    let color = prompt("Enter a new color for the poem");
+    let poem = document.getElementById("poem")
+    poem.style.color = color;
+}
+
+function changeFontSize() {
+    let 
+}
